bccbb8c5cbe5292f50e74ad400bd8c6057938d88
23CABSUBASHS <139529721+23CABSUBASHS@users.noreply.github.com>
2024-12-11 09:45:40 +0000

    p1

--- test.java
+++ test.java
@@ -1,19 +1,22 @@
-DSA TEST PROGRAM
-
 import java.util.Scanner;
 
 class test {
-  public static void main(String[] args) {
+  public static void main(String args[]) {
     Scanner myObj = new Scanner(System.in);
 
     System.out.println("Enter name, age and salary:");
 
     String name = myObj.nextLine();
-    // 
-       TYPE YOUR CODE HERE
-      //
+
+    int age=myObj.nextInt();
+    double salary=myObj.nextDouble();
+
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
     System.out.println("Salary: " + salary);
+
+    Boolean isTrue=true;
+    long longValue=43903;
+    char singleCharacter='a';
   }
 }
